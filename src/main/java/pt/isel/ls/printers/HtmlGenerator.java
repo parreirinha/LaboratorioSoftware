05f2c321803f6061d39affdca4b665cfb7549533
tedemorgado <pitbull.tede@gmail.com>
2016-04-27 21:21:51 +0100

    Refractor Functions to ArrayList<Functions<T, String>>

--- src/main/java/pt/isel/ls/printers/HtmlGenerator.java
+++ src/main/java/pt/isel/ls/printers/HtmlGenerator.java
@@ -1,6 +1,6 @@
 package pt.isel.ls.printers;
 
-
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.function.Function;
 
@@ -25,14 +25,14 @@ public class HtmlGenerator<T>
             "\t\t</body>\n" +
             "\t</html>";
 
-    public static <T> String htmlGenerate(Collection<T> col, String[] head, Function<T, String>[] func)
+    public static <T> String htmlGenerate(Collection<T> col, String[] head, ArrayList<Function<T, String>> func)
     {
         if(col.size() == 1)
             return String.format(template, getText(col, head, func));
         return String.format(template, getTable(col, head, func));
     }
 
-    private static <T> String getTable(Collection<T> col, String[] head, Function<T, String>[] func)
+    private static <T> String getTable(Collection<T> col, String[] head, ArrayList<Function<T, String>> func)
     {
         String str = "\t\t\t<table border=\"1\" style=\"width:100%\">\n" + getHead(head);
         for(T t : col)
@@ -48,15 +48,15 @@ public class HtmlGenerator<T>
         return str;
     }
 
-    private static <T> String getText(Collection<T> col, String[] head, Function<T, String>[] func)
+    private static <T> String getText(Collection<T> col, String[] head, ArrayList<Function<T, String>> func)
     {
         T t = col.iterator().next();
         String str = "\t\t\t<ul>\n" +
-                "\t\t\t\t<li>"+head[0]+": "+func[0].apply(t)+"</li>\n" +
+                "\t\t\t\t<li>"+head[0]+": "+func.get(0).apply(t)+"</li>\n" +
                 "\t\t\t\t<ul>\n";
         for(int i = 1; i < head.length; ++i)
         {
-            str += "\t\t\t\t\t<li>"+head[i]+": "+func[i].apply(t)+"</li>\n";
+            str += "\t\t\t\t\t<li>"+head[i]+": "+func.get(i).apply(t)+"</li>\n";
         }
         str += "\t\t\t\t</ul>\n" +
                 "\t\t\t</ul>\n";
