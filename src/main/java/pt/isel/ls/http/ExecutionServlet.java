6c5a5e70bf8db259c7247986bc2c85ead66e5cba
ISEL-A40569 <40569@alunos.isel.ipl.pt>
2016-05-18 14:58:06 +0100

    UriCommandGetter implementation

--- src/main/java/pt/isel/ls/http/ExecutionServlet.java
+++ src/main/java/pt/isel/ls/http/ExecutionServlet.java
@@ -2,7 +2,9 @@ package pt.isel.ls.http;
 
 import pt.isel.ls.database.connection.ConnectionFactory;
 import pt.isel.ls.exceptions.ApplicationException;
+import pt.isel.ls.executioncommands.GetAllMovies;
 import pt.isel.ls.executioncommands.GetMovie;
+import pt.isel.ls.linecommand.mapping.CommandMapper;
 import pt.isel.ls.linecommand.model.Command;
 import pt.isel.ls.linecommand.process.CommandGetter;
 import pt.isel.ls.printers.Printable;
@@ -15,26 +17,36 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 
+import static pt.isel.ls.user.io.Run.identifyOutputFormat;
+
 /**
  * Created by Dani on 17-05-2016.
  */
-public class ExampleServlet extends HttpServlet {
+public class ExecutionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-// Set response content type
-        resp.setContentType("text/html");
 
-        // Actual logic goes here.
+        System.out.println("HERE "+req.getRequestURI());//gets path
+        System.out.println(req.getQueryString());//gets headers(?) and parameters //TODO check how headers is placed in uri
+
+
         PrintWriter out = resp.getWriter();
-        String[] commandline = {"GET", "/movies/1"};
-        Command c = new CommandGetter().getCommand(commandline);
+
+        Command c = new UriCommandGetter().getCommandFromUri("GET", req.getRequestURI(), req.getQueryString());
+
         Printable p = null;
         try {
-            p = new GetMovie().execute(new ConnectionFactory().getNewConnection(), c);
+            p = new CommandMapper().getExecutionCommandInstance(c).execute(
+              new ConnectionFactory().getNewConnection(), c);
         } catch (SQLException e) {
-            e.printStackTrace();
+            e.printStackTrace();//TODO tratar estas excepçoes
         } catch (ApplicationException e) {
             e.printStackTrace();
         }
-        out.println( "<a href=http://www.w3schools.com/html/>Visit our HTML tutorial</a> "); }
+
+        resp.setContentType(identifyOutputFormat(c,p));
+
+
+
+        out.println( p.toStringHtml()); }
 }
--- src/main/java/pt/isel/ls/http/HttpServer.java
+++ src/main/java/pt/isel/ls/http/HttpServer.java
@@ -6,14 +6,14 @@ import org.eclipse.jetty.servlet.ServletHandler;
  * Created by Dani on 17-05-2016.
  */
 public class HttpServer {
-    private static final int LISTEN_PORT = 8090;
+    private static final int LISTEN_PORT = 8080;
 
     public static void main(String[] args) throws Exception {
 
         Server server = new Server(LISTEN_PORT);
         ServletHandler handler = new ServletHandler();
         server.setHandler(handler);
-        handler.addServletWithMapping(ExampleServlet.class, "/*");
+        handler.addServletWithMapping(ExecutionServlet.class, "/*");
         server.start();
         System.out.println("Server is started");
 
