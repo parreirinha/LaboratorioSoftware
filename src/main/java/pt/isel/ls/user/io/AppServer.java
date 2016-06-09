1767c04ad087460d3ab62f49f3c2978988444058
tedemorgado <pitbull.tede@gmail.com>
2016-06-09 21:14:37 +0100

    testing

--- src/main/java/pt/isel/ls/user/io/AppServer.java
+++ src/main/java/pt/isel/ls/user/io/AppServer.java
@@ -16,26 +16,14 @@ public class AppServer
 {
     private static final Logger _logger = LoggerFactory.getLogger(AppServer.class);
     public static void main(String[] args) throws Exception {
-
-
-        //System.setProperty("org.slf4j.simpleLogger.levelInBrackets","true");
         _logger.info("------------------------PORT = '{}'-----------------------", System.getenv("PORT"));
         Server server = new Server(Integer.valueOf(System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
-        //context.addServlet(new ServletHolder(new TimeServlet()),"/*");
         context.addServlet(new ServletHolder(new ExecutionServlet()),"/*");
         server.start();
         server.join();
-        /*String portDef = System.getenv("PORT");
-        int port = portDef != null ? Integer.valueOf(portDef) : 8080;
-    	Server server = new Server(port);
-        ServletHandler handler = new ServletHandler();
-        server.setHandler(handler);
-        handler.addServletWithMapping(new ServletHolder(new ExecutionServlet()), "/");
-        server.start();
-        server.join();*/
 
     }
 }
